5504c8400fd1f49b3872e19134a7ea5943eba5fc
JLViz <jean.luc-yao@idinsinsight.org>
2023-11-25 14:07:29 +0000

    fixing spacing from bootstrap theme

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ headers  = {
     'Accept-Language': 'en-US,en;q=0.9',
 }
 ## Seasons available 
-season_list = ['1996-97','1997-98','1998-99','1999-00','2000-01','2001-02','2002-03','2003-04','2004-05','2005-06','2006-07','2007-08','2008-09','2009-10','2010-11','2011-12','2012-13','2013-14','2014-15','2015-16','2016-17','2017-18','2018-19','2019-20']
+season_list = ['2020-21','2021-22','2022-23','2023-24']
 
 ## Headers of the dataframe
 heads = ["PLAYER_ID", "PLAYER_NAME", "NICKNAME", "TEAM_ID", "TEAM_ABBREVIATION", "AGE", "GP", "W", "L","W_PCT","MIN","FGM","FGA","FG_PCT","FG3M","FG3A","FG3_PCT","FTM","FTA","FT_PCT","OREB","DREB","REB","AST","TOV","STL","BLK","BLKA","PF","PFD","PTS","PLUS_MINUS","NBA_FANTASY_PTS","DD2","TD3","GP_RANK","W_RANK","L_RANK","W_PCT_RANK","MIN_RANK","FGM_RANK","FGA_RANK","FG_PCT_RANK","FG3M_RANK","FG3A_RANK","FG3_PCT_RANK","FTM_RANK","FTA_RANK","FT_PCT_RANK","OREB_RANK","DREB_RANK","REB_RANK","AST_RANK","TOV_RANK","STL_RANK","BLK_RANK","BLKA_RANK","PF_RANK","PFD_RANK","PTS_RANK","PLUS_MINUS_RANK","NBA_FANTASY_PTS_RANK","DD2_RANK","TD3_RANK","CFID","CFPARAMS"]
@@ -49,7 +49,7 @@ for season in season_list :
     data.columns = heads
 
     ## Save as csv file
-    name = season+'.csv'
+    name = 'Data/'+season+'.csv'
     data.to_csv(name)
     print("\n")
     print(season+" done.")
