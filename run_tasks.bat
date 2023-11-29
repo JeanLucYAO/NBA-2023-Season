7224e79ae831b2b83c8d18a6429712b1d856b707
JLViz <jean.luc-yao@idinsinsight.org>
2023-11-29 23:47:12 +0000

    Automated commit for data document update

--- run_tasks.bat
+++ run_tasks.bat
@@ -1,7 +1,17 @@
 @echo off
 
+EM Set the path to the log file.
+set LOGFILE=python_log.txt
+
+REM Get the current date and time to timestamp the log entry
+for /f "tokens=2 delims==" %%i in ('wmic OS Get localdatetime /value') do set datetime=%%i
+set datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
+
+REM Add a header to the log file with the current date and time
+echo [Start Time: %datetime%] >> %LOGFILE%
+
 REM Run the Python script
-python "%USERPROFILE%\Documents\NBA-2023-Season\script.py" > "%USERPROFILE%\Documents\NBA-2023-Season\python_log.txt" 2>&1
+python "%USERPROFILE%\Documents\NBA-2023-Season\script.py" >> %LOGFILE% 2>&1
 
 REM locate in repository
 cd /d "%USERPROFILE%\Documents\NBA-2023-Season"
@@ -18,3 +28,10 @@ git commit -m "Automated commit for data document update"
 
 REM Push the changes to GitHub
 git push
+
+
+echo [End Time: %datetime%] >> %LOGFILE%
+echo. >> %LOGFILE%
+
+echo Download finished.
+echo Check the log file (%LOGFILE%) for details.
